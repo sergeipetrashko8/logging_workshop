75b20d954a1d72e3554ec2bbf723c7ff3328bf2f
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-03-24 13:34:19 +0300

    Done customer service and controller.

---
+++ LoggingSample/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using LoggingSample_BLL.LogTargets;
+using NLog.Targets;
+
+namespace LoggingSample
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Target.Register<DbTarget>("DbTarget");
+        }
+    }
+}
---
+++ LoggingSample_DAL/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using LoggingSample_DAL.Configuration;
+using LoggingSample_DAL.Entities;
+
+namespace LoggingSample_DAL.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext() : base("LoggingSampleAppData")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Migrations.Configuration>());
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations
+                .Add(new CustomerConfiguration())
+                .Add(new OrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
